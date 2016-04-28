33908f53df17ff924646ae6d2750752c9768785a
Bohdan Nikolaienko <23bohdan@gmail.com>
2016-04-28 10:44:48 +0300

    Update worker.rb

--- lib/worker.rb
+++ lib/worker.rb
@@ -10,9 +10,9 @@ class Worker
   @@task_handler = Handler.new
 
   def perform(task_id)
-  	task = Task.find task_id
+    task = Task.find task_id
     task.status = :in_progress
-  	task.save!
+    task.save!
     @@task_handler.handle(task)
   end
 
