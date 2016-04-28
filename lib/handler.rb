9aa98b3efccafc2b1acfbc6f6dd73e258fa6c81c
Bohdan Nikolaienko <23bohdan@gmail.com>
2016-04-28 10:47:03 +0300

    Update handler.rb

--- lib/handler.rb
+++ lib/handler.rb
@@ -3,22 +3,22 @@ require 'mini_magick'
 class Handler
   
   def handle(task)
-  	image = MiniMagick::Image.open(task.image.url)
-  	operation = choose_operation(task.operation)
-  	result = operation.call(image, task.params)
-  	task.result = result
+    image = MiniMagick::Image.open(task.image.url)
+    operation = choose_operation(task.operation)
+    result = operation.call(image, task.params)
+    task.result = result
     task.status = :done
-  	task.save!
+    task.save!
   end
 
   def choose_operation(operation_name)
-  	lambda do |image, params|
-  	  case operation_name
-  	    when :resize then resize(image, params)
-  	    when :rotate then rotate(image, params)
+    lambda do |image, params|
+      case operation_name
+        when :resize then resize(image, params)
+        when :rotate then rotate(image, params)
         when :negate then negate(image)
-  	  end
-  	end
+      end
+    end
   end
 
   def resize(image, params)
@@ -26,7 +26,7 @@ class Handler
   end
 
   def rotate(image, params)
-  	image.rotate(params.values.first)
+    image.rotate(params.values.first)
   end
 
   def negate(image)
